fc13baacd0b767831e70963131f7066bd3cb83d3
Andrew Rodgers <root@debian.(none)>
2013-02-25 23:29:10 -0500

    not quite as broken, more extensible

--- main.py
+++ main.py
@@ -1,19 +1,17 @@
 from jinja2 import Environment, FileSystemLoader, meta
 import sys, string, random, requests, gzip
-from stringIO import stringIO
 
 values_used = {}
 def main():
-    print(sys.argv[1])
+    """Opens the recipe file specified on the command line, the calls the defined handlers for each instruction"""
     recipe_file = open('recipes/'+sys.argv[1])
     recipe = recipe_file.readlines()
     recipe = [line.rstrip().split(' ')for line in recipe]
     for line in recipe :
-        if line[0] == 'template':
-            fill_template(line[1])
-        print(values_used)
+        globals()[line[0]](line)
 
 def mine_salt(char):
+    """Generates random strings of char length for salts and keys, loaded as a Jinja filter in the fill_template method"""
     chars = string.uppercase + string.lowercase + string.digits + '%&!@#$ '
     salt = [random.choice(chars) for x in range(char)]
     return ''.join(salt)
@@ -22,10 +20,13 @@ def tar_get(url):
     r = requests.get(url)
     tar_file = open(stringIO(r.content))
 
-def fill_template(template_name):
+def fill_template(param_list):
     env = Environment(loader=FileSystemLoader(('nginx-templates/', 'mysql-templates/', 'temp/', 'misc-templates/')))
     env.filters['mine_salt'] = mine_salt
-    template_source = env.loader.get_source(env, template_name)[0]
+    try:
+        template_source = env.loader.get_source(env, param_list[1])[0]
+    except:
+        raise NameError('Template Name invalid, please check recipe')
     template = env.get_template(template_name)
     parsed_content = env.parse(template_source)
     tags = meta.find_undeclared_variables(parsed_content)
